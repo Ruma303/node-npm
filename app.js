c8d0eb16aea9ad4378581d032ce174b350c828dc
Ruma303 <matteoh8@hotmail.it>
2023-10-25 18:13:58 +0200

    require vs import

--- app.js
+++ app.js
@@ -10,3 +10,40 @@ console.log(val3)
 
 const db = require('./db.json')
 console.log(db)
+
+
+//% require vs import
+
+//# require()
+/* // importare moduli da node_modules
+const fs = require('fs');
+// importare file
+const db = require('./db.json')
+
+module.exports = function test () {
+    console.warn('This is a test!')
+} */
+
+
+
+//# import / from
+/* button.addEventListener('click', async () => {
+    const module = await import('./myModule.js');
+    module.myFunction();
+}); */
+
+/* // importare moduli da node_modules
+import debug from 'debug';
+// importare altri file
+import db from './db.json'
+// alias
+import { isBoolean as bool } from 'validator'
+
+export const a = 10;
+export const b = 20;
+export const c = () => 'Hi!';
+export default hello = 'Hello World';
+
+import { a, b, c } from './'
+import hello from './' */
+
