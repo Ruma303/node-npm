2e5d23b175b02a77128a771e412b308a6a31b8d3
Ruma303 <matteoh8@hotmail.it>
2023-10-30 21:03:13 +0100

    password-generator

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const validator = require('validator')
+//const validator = require('validator')
 //console.log(validator)
 
-const val1 = validator.isBoolean('true')
+
+/* const val1 = validator.isBoolean('true')
 const val2 = validator.isBoolean('24')
 console.log(val1, val2)
 const val3 = validator.blacklist('<p>\'Codice da sanificare.\'</p>', '<>/')
@@ -9,7 +10,7 @@ console.log(val3)
 
 
 const db = require('./db.json')
-console.log(db)
+console.log(db) */
 
 
 //% require vs import
